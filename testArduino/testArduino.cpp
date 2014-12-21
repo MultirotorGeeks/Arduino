923710f0c55e4660ded607619971f48ab55e62de
Paul Rancuret <pmrancuret@gmail.com>
2014-12-21 10:58:58 -0500

    Added call to the ArduRadios library, in order to read the roll, pitch, throttle, and yaw values from the radio.  Set up the loop so that these values will be periodically displayed.

--- testArduino/testArduino.cpp
+++ testArduino/testArduino.cpp
@@ -5,7 +5,8 @@
  *      Author: Paul
  */
 
-#include "Arduino.h"
+#include <Arduino.h>
+#include "ArduRadios.h"
 
 
 //end of add your includes here
@@ -18,15 +19,63 @@ void setup();
 } // extern "C"
 #endif
 
+#define LOOPTIME_MICROSEC 100000
+#define RADIOFILTCOEFF_NA 20
+#define SERIALBAUD 38400
 
 void loop(void)
 {
+  uint16_t radiovals[4]; // values read from the radio
+
+  static unsigned long lastTime = 0; // time of last loop iteration
+  unsigned long thisTime = micros(); // time of this loop iteration
+
+  if ( thisTime - lastTime >= LOOPTIME_MICROSEC ) // if enough time has elapsed
+  {
+    if ( ArduRadios.read_radio(radiovals) == RADIO_RTN_SUCCESS ) // read radios
+    {
+      /*
+       * Print Radio Values when a successfully read
+       */
+      Serial.print("Time: ");
+      Serial.print( ( thisTime / 1000000 ) , DEC );
+      Serial.print(".");
+      Serial.print( ( ( thisTime % 1000000 ) / 10000 ) , DEC );
+      Serial.print(",  Roll: ");
+      Serial.print( radiovals[CH_ROLL] , DEC );
+      Serial.print(",  Pitch: ");
+      Serial.print( radiovals[CH_PITCH] , DEC );
+      Serial.print(",  Throttle: ");
+      Serial.print( radiovals[CH_THROTTLE] , DEC );
+      Serial.print(",  Rudder: ");
+      Serial.println( radiovals[CH_RUDDER] , DEC );
+    }
+    else // problem occurred reading radios
+    {
+      /*
+       * Print Error Message when radio read failed
+       */
+      Serial.print("Time: ");
+      Serial.print( ( thisTime / 1000000 ) , DEC );
+      Serial.print(".");
+      Serial.print( ( ( thisTime % 1000000 ) / 10000 ) , DEC );
+      Serial.println(",  Error Reading Radio Values!" );
+    }
+
+  }
+
+  lastTime = thisTime;
 
   return;
 }
 
 void setup(void)
 {
+  uint8_t filterCoeffs[4] = {RADIOFILTCOEFF_NA,RADIOFILTCOEFF_NA,
+      RADIOFILTCOEFF_NA,RADIOFILTCOEFF_NA}; // initialize filter coefficients
+
+  Serial.begin(SERIALBAUD); // begin serial communications
+  ArduRadios.groundInitFilt(filterCoeffs); // initialize radio inputs
 
   return;
 }
