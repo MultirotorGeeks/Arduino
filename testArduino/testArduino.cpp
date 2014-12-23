52099ce1510586195e6fd3b0d8602b533d6ffc54
Paul Rancuret <pmrancuret@gmail.com>
2014-12-22 19:17:27 -0500

    Used uncrustify to apply gnu code style.

--- testArduino/testArduino.cpp
+++ testArduino/testArduino.cpp
@@ -25,44 +25,43 @@ void setup();
 
 void loop(void)
 {
-  uint16_t radiovals[4]; // values read from the radio
+  uint16_t radiovals[4];       // values read from the radio
 
-  static unsigned long lastTime = 0; // time of last loop iteration
-  unsigned long thisTime = micros(); // time of this loop iteration
+  static unsigned long lastTime = 0;       // time of last loop iteration
+  unsigned long thisTime = micros();       // time of this loop iteration
 
-  if ( thisTime - lastTime >= LOOPTIME_MICROSEC ) // if enough time has elapsed
-  {
-    if ( ArduRadios.read_radio(radiovals) == RADIO_RTN_SUCCESS ) // read radios
+  if (thisTime - lastTime >= LOOPTIME_MICROSEC)         // if enough time has elapsed
     {
-      /*
-       * Print Radio Values when a successfully read
-       */
-      Serial.print("Time: ");
-      Serial.print( ( thisTime / 1000000 ) , DEC );
-      Serial.print(".");
-      Serial.print( ( ( thisTime % 1000000 ) / 10000 ) , DEC );
-      Serial.print(",  Roll: ");
-      Serial.print( radiovals[CH_ROLL] , DEC );
-      Serial.print(",  Pitch: ");
-      Serial.print( radiovals[CH_PITCH] , DEC );
-      Serial.print(",  Throttle: ");
-      Serial.print( radiovals[CH_THROTTLE] , DEC );
-      Serial.print(",  Rudder: ");
-      Serial.println( radiovals[CH_RUDDER] , DEC );
+      if (ArduRadios.read_radio(radiovals) == RADIO_RTN_SUCCESS)             // read radios
+        {
+          /*
+           * Print Radio Values when a successfully read
+           */
+          Serial.print("Time: ");
+          Serial.print((thisTime / 1000000), DEC);
+          Serial.print(".");
+          Serial.print(((thisTime % 1000000) / 10000), DEC);
+          Serial.print(",  Roll: ");
+          Serial.print(radiovals[CH_ROLL], DEC);
+          Serial.print(",  Pitch: ");
+          Serial.print(radiovals[CH_PITCH], DEC);
+          Serial.print(",  Throttle: ");
+          Serial.print(radiovals[CH_THROTTLE], DEC);
+          Serial.print(",  Rudder: ");
+          Serial.println(radiovals[CH_RUDDER], DEC);
+        }
+      else           // problem occurred reading radios
+        {
+          /*
+           * Print Error Message when radio read failed
+           */
+          Serial.print("Time: ");
+          Serial.print((thisTime / 1000000), DEC);
+          Serial.print(".");
+          Serial.print(((thisTime % 1000000) / 10000), DEC);
+          Serial.println(",  Error Reading Radio Values!");
+        }
     }
-    else // problem occurred reading radios
-    {
-      /*
-       * Print Error Message when radio read failed
-       */
-      Serial.print("Time: ");
-      Serial.print( ( thisTime / 1000000 ) , DEC );
-      Serial.print(".");
-      Serial.print( ( ( thisTime % 1000000 ) / 10000 ) , DEC );
-      Serial.println(",  Error Reading Radio Values!" );
-    }
-
-  }
 
   lastTime = thisTime;
 
@@ -71,11 +70,11 @@ void loop(void)
 
 void setup(void)
 {
-  uint8_t filterCoeffs[4] = {RADIOFILTCOEFF_NA,RADIOFILTCOEFF_NA,
-      RADIOFILTCOEFF_NA,RADIOFILTCOEFF_NA}; // initialize filter coefficients
+  uint8_t filterCoeffs[4] = { RADIOFILTCOEFF_NA, RADIOFILTCOEFF_NA,
+                              RADIOFILTCOEFF_NA, RADIOFILTCOEFF_NA };    // initialize filter coefficients
 
-  Serial.begin(SERIALBAUD); // begin serial communications
-  ArduRadios.groundInitFilt(filterCoeffs); // initialize radio inputs
+  Serial.begin(SERIALBAUD);       // begin serial communications
+  ArduRadios.groundInitFilt(filterCoeffs);       // initialize radio inputs
 
   return;
 }
